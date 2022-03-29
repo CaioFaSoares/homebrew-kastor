448e9c73eb1aff9f11852703421d5d7d7b418676
Caio <51139215+CaioFaSoares@users.noreply.github.com>
2022-03-29 12:50:39 -0300

    Update kastor.rb

--- kastor.rb
+++ kastor.rb
@@ -2,7 +2,7 @@ class Kastor < Formula
   desc "Easily customize your MacOSX dock. Built on Swift."
   homepage "https://github.com/CaioFaSoares/kastor"
   url "https://github.com/CaioFaSoares/kastor/releases/download/v1.0.0/kastor"
-  sha256 "eb0437eafbe99233065e400b5e32c109ddaf1ff82145b104c97a04fe24957468"
+  sha256 "15291ac67d54ce278fc2ecf915d5fc5a9ee2c6d4a60ea3b39941fa3bc29a7989"
   license "MIT"
   
   def install
