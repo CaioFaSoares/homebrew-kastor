8ce2a51d7c1ee9441cc471eb1247baee7fa4a195
Caio <51139215+CaioFaSoares@users.noreply.github.com>
2022-03-29 08:37:54 -0300

    Update kastor.rb

--- kastor.rb
+++ kastor.rb
@@ -4,6 +4,7 @@ class Kastor < Formula
   url "https://github.com/CaioFaSoares/kastor/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "eb0437eafbe99233065e400b5e32c109ddaf1ff82145b104c97a04fe24957468"
   license "MIT"
+  
   def install
     system "./configure", *std_configure_args, "--disable-silent-rules"
   end
